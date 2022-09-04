d6a198416e72b605329a904335bc6514b6e835ef
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-04 10:46:27 -0400

    commit tree 322c1ac2ec691fcc1025ceefc6b4a21918f82504

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+    return customerName = customerName.toUpperCase();
+}
+var bestCustomer
+
+function setBestCustomer(){
+bestCustomer ='not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Max'
+
+function changeLeastFavoriteCustomer (){
+    return leastFavoriteCustomer = 'Anthony'
+}
